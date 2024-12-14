e46fcdc7dd310588ec45a57ce31f16dbb671fc39
AMIT RANA <123amitrana0123@gmail.com>
2024-12-14 14:19:39 +0530

    feat: add dynamic template generation for Git hooks

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,20 @@
 #!/usr/bin/env node
 
-console.log("testing the script!")
+const { program } = require('commander');
+const { addHooks } = require('../lib/hooks');
+
+program
+    .name('gitpodify')
+    .description('CLI utility to set up Git hooks and configurations')
+    .version('1.0.0');
+
+// hooks command
+
+program
+    .command('add hooks')
+    .description('Add Git hooks with predefined templates')
+    .action(() => {
+        addHooks()
+    })
+
+program.parse(process.argv);
